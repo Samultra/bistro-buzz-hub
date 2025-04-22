822c4abbefd4e1b1dfc4a8c61d3f45c2464f22bb
samultra <samultra7@gmail.com>
2025-04-22 22:38:58 +0300

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import RestaurantDetail from "./pages/RestaurantDetail";
 import NotFound from "./pages/NotFound";
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
@@ -30,12 +30,10 @@ const App = () => (
           <Route path="/contacts" element={<Contacts />} />
           <Route path="/restaurant/:id" element={<RestaurantDetail />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/bistro-buzz-hub" element={<Navigate to="/" replace />} />
-          <Route path="/bistro-buzz-hub/*" element={<Navigate to="/" replace />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
